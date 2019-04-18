6356d4f90c1b40a96c2840646b1e456a0c90ae7f
Adella <adellakenn@gmail.com>
2019-04-18 23:14:36 +0000

    Automatically backed up by Learn

---
+++ loops0.js
@@ -0,0 +1,14 @@
+//build a function forLoop. 
+//takes an array as an argument
+function forLoop(array) { 
+ for(let i = 0; i < 25; i++)
+  if (i === 1) {
+  array.push `I am 1 strange loop.`
+  } else {
+    array.push `I am ${i} strange loops.`
+  }
+  return (array)
+}
+
+
+
